64cf254dc52defaec55a4007c3aba79a73a45b1a
Olivier Lemasle <o.lemasle@gmail.com>
2016-10-02 10:47:51 +0200

    Add Travis CI

---
+++ distribution/publish-snapshot.sh
@@ -0,0 +1,12 @@
+# see https://coderwall.com/p/9b_lfq
+
+if [ "$TRAVIS_REPO_SLUG" == "olivierlemasle/plaintext-maven-plugin" ] && \
+   [ "$TRAVIS_PULL_REQUEST" == "false" ] && \
+   [ "$TRAVIS_BRANCH" == "master" ]; then
+
+  echo -e "Publishing maven snapshot...\n"
+
+  mvn clean source:jar deploy --settings="build/settings.xml" -Dmaven.javadoc.skip=true
+
+  echo -e "Published maven snapshot"
+fi
